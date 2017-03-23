bc20d9419422e12d96926372194f177d4cbea5d3
jclc <jclc@protonmail.com>
2017-03-23 15:18:15 +0200

    added testing framework

--- include/output.hpp
+++ include/output.hpp
@@ -2,6 +2,19 @@
 
 class Output {
 public:
-	virtual bool openFile(std::string& fileName);
-	virtual void write(const char* data, int buffer);
+	virtual bool openFile(std::string& fileName) {}
+	virtual void write(const char* data, int bufferSize) = 0;
+};
+
+class FileOutput: public Output {
+public:
+	FileOutput();
+	bool openFile(std::string& fileName);
+	void write(const char *data, int bufferSize);
+};
+
+class StdOutput: public Output {
+public:
+	StdOutput();
+	void write(const char *data, int bufferSize);
 };
---
+++ tests/testtest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(test_test_case, test_test) {
+	ASSERT_TRUE(1);
+}
